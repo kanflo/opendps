82f0c9280ed3ec7fcc12726dc868cd90da3e4a52
Johan Kanflo <johan.kanflo@bitfuse.net>
2017-07-28 23:53:07 +0200

    Past dumper

---
+++ pastdump.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Dump past area to pastdump.bin. Requires OpenOCD
+#
+arm-none-eabi-gdb -ex "target remote localhost:3333" -ex "monitor reset halt" -ex "dump binary memory pastdump.bin 0x0800f800 0x08010000" -ex "quit" --batch
+hexdump -C pastdump.bin
