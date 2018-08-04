c1b897bb5c7b0178dbcbff1ce44df5b70f00c8c5
Aaron Keith <aaron.keith@compacsort.com>
2018-08-04 14:44:54 +1200

    Revert "First check in of the simulator"
    This reverts commit 7544b60a55afb0661865b088fe80395e9a48d21c.
    Should have push to a branch

--- simulator/run_qmake.sh
+++
@@ -1,7 +0,0 @@
-"C:\Qt\Qt5.5.1\5.5\mingw492_32\bin\qmake.exe" *.pro -r -spec win32-g++ "CONFIG+=debug"
-
-#need to add path to find dlls
-#export PATH=$PATH:"/C/Qt/Qt5.5.1/5.5/mingw492_32/bin"
-#make -f Makefile.Debug
-#in CDT Add to the PATH in the luncher  ${env_var:PATH};C:\Qt\Qt5.5.1\5.5\mingw492_32\bin
-
