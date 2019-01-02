e53a7b6e6ad45699b68f15d3de4c0ce9cd972583
Nick Brassel <nick@tzarc.org>
2019-01-02 01:48:36 +0000

    Restore make args, faster docker builds.
    - Make args weren't able to be changed when building with Docker. Added capability of specifying them as arguments to the build_binaries.sh script.
    - Made the docker builds faster by virtue of not doing any extra work during the container build stage, much faster turnaround time for builds during development.

--- build_binaries.sh
+++ build_binaries.sh
@@ -2,16 +2,30 @@
 
 # Builds OpenDPS binaries in docker and then copies it over to host machine
 
-set -e
-git rev-parse --short HEAD
+set -eEuo pipefail
+umask 022
+
+declare CONTAINER_ID
+cleanup() {
+    if [ -n ${CONTAINER_ID:-} ] ; then
+        docker stop $CONTAINER_ID \
+            || docker kill $CONTAINER_ID \
+            || true
+        docker rm $CONTAINER_ID \
+            || true
+    fi
+}
+trap cleanup EXIT HUP INT
+
 PROJECT=$(git rev-parse --short HEAD)
 echo "Building opendps git version: $PROJECT"
-docker build -t $PROJECT -f Dockerfile .
 
-CONTAINER_ID=$(docker create $PROJECT bash)
-echo "CONTAINER ID: $CONTAINER_ID"
+docker rmi --no-prune opendps_$PROJECT || true
+docker build -t opendps_$PROJECT docker
+
+CONTAINER_ID=$(docker run -td -u opendps -v $(readlink -f .):/parent:ro opendps_$PROJECT bash)
+docker exec -u opendps $CONTAINER_ID /build.sh "$@"
 docker cp $CONTAINER_ID:/home/opendps/code/opendps/opendps.elf ./opendps/opendps.elf
 docker cp $CONTAINER_ID:/home/opendps/code/opendps/opendps.bin ./opendps/opendps.bin
 docker cp $CONTAINER_ID:/home/opendps/code/dpsboot/dpsboot.elf ./dpsboot/dpsboot.elf
 docker cp $CONTAINER_ID:/home/opendps/code/dpsboot/dpsboot.bin ./dpsboot/dpsboot.bin
-docker rm $CONTAINER_ID
---
+++ docker/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eEuo pipefail
+umask 022
+
+sudo mkdir -p /home/opendps/code
+sudo cp -R /parent/{*,.git} /home/opendps/code
+sudo chown -R opendps:opendps /home/opendps
+
+cd /home/opendps/code
+git submodule init
+git submodule update
+make -j -C libopencm3
+make -C opendps elf bin "$@"
+make -C dpsboot elf bin "$@"
