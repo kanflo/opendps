8b70593aa19269a3dbf125fa8bf81b9bdac20a61
Johan Kanflo <johan.kanflo@bitfuse.net>
2019-01-06 21:57:35 +0100

    Fixed #81

--- dpsctl/dpsctl.py
+++ dpsctl/dpsctl.py
@@ -493,7 +493,7 @@ def run_upgrade(comms, fw_file_name, args):
                 fail("device reported an unknown error (%d)" % status)
     else:
         fail("Device rejected firmware upgrade")
-    sys.exit(os.EX_OK)
+    sys.exit(0)
 
 """
 Create and return a comminications interface object or None if no comms if
