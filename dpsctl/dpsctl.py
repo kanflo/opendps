7ec51d29490935f4035c58b29a648ce1c6399daa
Joshua Booth <jboothsoftware@gmail.com>
2019-02-10 22:48:39 +0000

    change constant current calibration to not use a load resistor

--- dpsctl/dpsctl.py
+++ dpsctl/dpsctl.py
@@ -888,9 +888,10 @@ def do_calibration(comms, args):
         plt.show()
 
     print("Constant Current Calibration:")
-    # Set the V_DAC value to our safe maximum so we can't over current our load
-    output_dac = int(round(max_output_voltage_mv * v_dac_k + v_dac_c))
-    args.parameter = ["V_DAC={}".format(output_dac)]
+    raw_input("Please short the output of the DPS with a thick wire capable of carrying {}A, then press enter".format(max_dps_current))
+
+    # Set the V_DAC output to the maximum
+    args.parameter = ["V_DAC={}".format(4095)]
     payload = create_set_parameter(args.parameter)
     communicate(comms, payload, args)
 
@@ -965,7 +966,7 @@ def do_calibration(comms, args):
         time.sleep(1)  # Wait for the DPS output to settle
 
         # Add these readings to our array
-        calibration_i_out.append((get_average_calibration_result(comms, 'vout_adc') * v_adc_k + v_dac_c) / load_resistance)
+        calibration_i_out.append((get_average_calibration_result(comms, 'iout_adc') * a_adc_k + a_adc_c))
         calibration_a_dac.append(output_dac)
 
     communicate(comms, create_enable_output("off"), args)  # Turn the output off
@@ -999,7 +1000,7 @@ def do_calibration(comms, args):
     communicate(comms, create_change_screen(protocol.CHANGE_SCREEN_MAIN), args)
 
     print("Calibration Complete\r\n")
-    print("To restore the device to factory defaults use dpsctl.py --calibration_reset")
+    print("To restore the device to the OpenDPS defaults use dpsctl.py --calibration_reset")
 
 
 def uhej_worker_thread():
