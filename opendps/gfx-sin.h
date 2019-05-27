3156e956d5e58e1dd298e324612893de98661608
X-Ryl669 <boite.pour.spam@gmail.com>
2019-05-27 19:46:12 +0200

    Start adding icons for function generator

---
+++ opendps/gfx-sin.h
@@ -0,0 +1,13 @@
+/** Gfx generated from gfx/png/sin.png */
+
+#ifndef __GFX_SIN_H__
+#define __GFX_SIN_H__
+
+#include <stdint.h>
+
+#define GFX_SIN_HEIGHT (15)
+#define GFX_SIN_WIDTH  (32)
+
+extern const uint8_t gfx_sin[960];
+
+#endif // __GFX_SIN_H__
---
+++ opendps/gfx-square.h
@@ -0,0 +1,13 @@
+/** Gfx generated from gfx/png/square.png */
+
+#ifndef __GFX_SQUARE_H__
+#define __GFX_SQUARE_H__
+
+#include <stdint.h>
+
+#define GFX_SQUARE_HEIGHT (15)
+#define GFX_SQUARE_WIDTH  (32)
+
+extern const uint8_t gfx_square[960];
+
+#endif // __GFX_SQUARE_H__
