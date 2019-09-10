b2c29f833fcb2a003428fbfdc2e68dd606c57610
Daniel Kucera <daniel.kucera@gmail.com>
2019-09-10 22:05:26 +0200

    travis 3

--- build_binaries.sh
+++ build_binaries.sh
@@ -23,9 +23,8 @@ echo "Building opendps git version: $PROJECT"
 docker rmi --no-prune opendps_$PROJECT || true
 docker build -t opendps_$PROJECT docker
 
-CONTAINER_ID=$(docker run -td -u opendps -v $(readlink -f .):/parent:ro opendps_$PROJECT bash)
+mkdir output
+chmod 777 output
+
+CONTAINER_ID=$(docker run -td -u opendps -v $(readlink -f .):/parent:ro -v $(readlink -f .)/output:/output opendps_$PROJECT bash)
 docker exec -u opendps $CONTAINER_ID /build.sh "$@"
-docker cp $CONTAINER_ID:/home/opendps/code/opendps/opendps.elf ./opendps/opendps.elf
-docker cp $CONTAINER_ID:/home/opendps/code/opendps/opendps.bin ./opendps/opendps.bin
-docker cp $CONTAINER_ID:/home/opendps/code/dpsboot/dpsboot.elf ./dpsboot/dpsboot.elf
-docker cp $CONTAINER_ID:/home/opendps/code/dpsboot/dpsboot.bin ./dpsboot/dpsboot.bin
