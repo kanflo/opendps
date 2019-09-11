fd4a0828fac464da4cf3bc95ccb5a8e307eeace4
root <root@mail.danman.eu>
2019-09-11 09:16:41 +0200

    mkdir -p

--- build_binaries.sh
+++ build_binaries.sh
@@ -11,7 +11,7 @@ echo "Building opendps git version: $PROJECT"
 docker rmi --no-prune opendps_$PROJECT || true
 docker build -t opendps_$PROJECT docker
 
-mkdir output || true
+mkdir -p output
 chmod 777 output
 
 docker run -t -u opendps -v $(readlink -f .):/parent:ro -v $(readlink -f .)/output:/output opendps_$PROJECT /parent/docker/build.sh "$@"
