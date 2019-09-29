f9f4ea9639a734cbae436d230b14a5a81335c0e7
Leo Leung <leo@steamr.com>
2019-09-29 15:05:31 -0600

    Added power limit feature to DPSMode. Maximum power can be defined and a warning icon will be displayed if output power exceeds 80%. Power will cut if it exceeds 100% and a flashing icon will display. Reset by pressing power button once.

---
+++ opendps/gfx-oppbar.h
@@ -0,0 +1,13 @@
+/** Gfx generated from gfx/png/oppbar.png */
+
+#ifndef __GFX_OPPBAR_H__
+#define __GFX_OPPBAR_H__
+
+#include <stdint.h>
+
+#define GFX_OPPBAR_HEIGHT (25)
+#define GFX_OPPBAR_WIDTH  (10)
+
+extern const uint8_t gfx_oppbar[500];
+
+#endif // __GFX_OPPBAR_H__
