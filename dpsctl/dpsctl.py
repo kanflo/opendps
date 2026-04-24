d3d6a5a672ad03a1cc0199e89104ebd295e8063e
Marian Buschsieweke <marian.buschsieweke@ml-pa.com>
2026-04-24 20:26:05 +0200

    dpsctl: allow setting device via env
    Adding `export DPSCTL_DEVICE=/dev/ttyXYZ` to your `~/.profile` /
    `~/.bashrc` / `~/.zshrc` / ... allos dropping the `-d /dev/ttyXZY`
    parameter. It is only passed in as default, so overwriting via
    command line remains possible. Not having that environment variable
    exported results in no change of behavior.

--- dpsctl/dpsctl.py
+++ dpsctl/dpsctl.py
@@ -1205,7 +1205,7 @@ def main():
     testing = '--testing' in sys.argv
     parser = argparse.ArgumentParser(description='Instrument an OpenDPS device')
 
-    parser.add_argument('-d', '--device', help="OpenDPS device to connect to. Can be a /dev/tty device, IP address for UDP protocol or tcp:IP for TCP protocol. If omitted, dpsctl.py will try the environment variable DPSIF", default='')
+    parser.add_argument('-d', '--device', help="OpenDPS device to connect to. Can be a /dev/tty device, IP address for UDP protocol or tcp:IP for TCP protocol. If omitted, dpsctl.py will try the environment variable DPSIF", default=os.getenv('DPSCTL_DEVICE'))
     parser.add_argument('-b', '--baudrate', type=int, dest="baudrate", help="Set baudrate used for serial communications", default=9600)
     parser.add_argument('-B', '--brightness', type=int, help="Set display brightness (0..100)")
     parser.add_argument('--set-baud', type=int, dest="set_baud", default=None,
